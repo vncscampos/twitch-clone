3061eee2ec7efc31cdc13c571b4f8b1d88e4209b
Vinicius Campos <viniolic2018@gmail.com>
2024-06-12 16:45:57 -0300

    creating navbar

--- app/(browse)/(home)/page.tsx
+++ app/(browse)/(home)/page.tsx
@@ -1,10 +1,7 @@
-import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
-      <p>Only authenticated users can se this</p>
-      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,12 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-const isProtectedRoute = createRouteMatcher(["/"]);
+const publicRoutes = createRouteMatcher(["/"])
 const clerkWebHooks = createRouteMatcher(["/api/webhooks(.*)"]);
 
 export default clerkMiddleware((auth, req) => {
   if (clerkWebHooks(req)) return NextResponse.next();
-  if (isProtectedRoute(req)) auth().protect();
+  if (publicRoutes(req)) return NextResponse.next();
 });
 
 export const config = {
