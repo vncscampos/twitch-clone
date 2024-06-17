a860608c1a0c5f332c90053478af2d3495af4d35
Vinicius Campos <viniolic2018@gmail.com>
2024-06-17 13:47:10 -0300

    creating ingress action

--- app/(dashboard)/u/[username]/keys/_components/connect-modal.tsx
+++ app/(dashboard)/u/[username]/keys/_components/connect-modal.tsx
@@ -1,56 +1,84 @@
 "use client";
 
+import { IngressInput } from "livekit-server-sdk";
+import { AlertTriangle } from "lucide-react";
+import { ElementRef, useRef, useState, useTransition } from "react";
+
+import { createIngress } from "@/actions/ingress";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Button } from "@/components/ui/button";
-import { Dialog, DialogHeader } from "@/components/ui/dialog";
-import { Select } from "@/components/ui/select";
 import {
-    DialogClose,
-    DialogContent,
-    DialogTitle,
-    DialogTrigger,
-} from "@radix-ui/react-dialog";
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import {
-    SelectContent,
-    SelectItem,
-    SelectTrigger,
-    SelectValue,
-} from "@radix-ui/react-select";
-import { AlertTriangle } from "lucide-react";
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { toast } from "sonner";
+
+const RTMP = String(IngressInput.RTMP_INPUT);
+const WHIP = String(IngressInput.WHIP_INPUT);
+
+type IngressType = typeof RTMP | typeof WHIP;
 
 export function ConnectModal() {
+  const closeRef = useRef<ElementRef<"button">>(null);
+  const [isPending, startTransition] = useTransition();
+  const [ingressType, setIngressType] = useState<IngressType>(RTMP);
+
+  const onSubmit = () => {
+    startTransition(() => {
+      createIngress(parseInt(ingressType))
+        .then(() => {
+          toast.success("Ingress created");
+          closeRef?.current?.click();
+        })
+        .catch(() => toast.error("Something went wrong."));
+    });
+  };
+
   return (
     <Dialog>
       <DialogTrigger asChild>
-        <Button variant="primary">Connection</Button>
+        <Button variant="primary">Generate Connection</Button>
       </DialogTrigger>
       <DialogContent>
         <DialogHeader>
-          <DialogTitle>Generate connection</DialogTitle>
+          <DialogTitle>Generate Connection</DialogTitle>
         </DialogHeader>
-        <Select>
+        <Select
+          disabled={isPending}
+          value={ingressType}
+          onValueChange={(value) => setIngressType(value)}
+        >
           <SelectTrigger className="w-full">
             <SelectValue placeholder="Ingress Type" />
           </SelectTrigger>
           <SelectContent>
-            <SelectItem value="RTMP">RTMP</SelectItem>
-            <SelectItem value="WHIP">WHIP</SelectItem>
+            <SelectItem value={RTMP}>RTMP</SelectItem>
+            <SelectItem value={WHIP}>WHIP</SelectItem>
           </SelectContent>
         </Select>
         <Alert>
-          <AlertTriangle className="w-4 h-4">
-            <AlertTitle>Warning!</AlertTitle>
-            <AlertDescription>
-              This action will reset all active streams using the current
-              connection
-            </AlertDescription>
-          </AlertTriangle>
+          <AlertTriangle className="h-4 w-4" />
+          <AlertTitle>Warning!</AlertTitle>
+          <AlertDescription>
+            This will reset all active streams using the current connection.
+          </AlertDescription>
         </Alert>
         <div className="flex justify-between">
-          <DialogClose>
-            <Button variant="ghost">Cancel</Button>
+          <DialogClose ref={closeRef} asChild>
+            <Button variant="destructive">Cancel</Button>
           </DialogClose>
-          <Button variant="primary" onClick={() => {}}>
+          <Button disabled={isPending} onClick={onSubmit} variant="primary">
             Generate
           </Button>
         </div>
