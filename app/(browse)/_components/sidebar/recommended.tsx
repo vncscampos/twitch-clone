2184e2b4ac10c73ba3a5f042dba55b691b717e48
Vinicius Campos <viniolic2018@gmail.com>
2024-06-18 13:47:50 -0300

    integrating livekit webhook

--- app/(browse)/_components/sidebar/recommended.tsx
+++ app/(browse)/_components/sidebar/recommended.tsx
@@ -1,11 +1,11 @@
 "use client";
 
 import { useSidebar } from "@/store/use-sidebar";
-import { User } from "@prisma/client";
+import { Stream, User } from "@prisma/client";
 import { UserItem, UserItemSkeleton } from "./user-item";
 
 interface RecommendedProps {
-  data: User[];
+  data: (User & { stream: Stream | null })[];
 }
 
 export function Recommended({ data }: RecommendedProps) {
@@ -24,7 +24,7 @@ export function Recommended({ data }: RecommendedProps) {
             key={user.id}
             username={user.username}
             imageUrl={user.imageUrl}
-            isLive={false}
+            isLive={user.stream?.isLive}
           />
         ))}
       </ul>
