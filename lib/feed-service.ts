1dbea5e5c4932357a32d1c6d181b0be8e48ede42
Vinicius Campos <viniolic2018@gmail.com>
2024-06-25 16:30:19 -0300

    creating user homepage

---
+++ lib/feed-service.ts
@@ -0,0 +1,52 @@
+import { getSelf } from "./auth-service";
+import { db } from "./db";
+
+export async function getStreams() {
+    let userId;
+
+    try {
+        const self = await getSelf();
+        userId = self.id;
+    } catch (error) {
+        userId = null;
+    }
+
+    let streams = [];
+
+    if (userId) {
+        streams = await db.stream.findMany({
+            where: {
+                user: {
+                    NOT: {
+                        blocking: {
+                            some: {
+                                blockedId: userId,
+                            },
+                        },
+                    },
+                },
+            },
+            select: {
+                id: true,
+                user: true,
+                thumbnailUrl: true,
+                name: true,
+                isLive: true,
+            },
+            orderBy: [{ isLive: "desc" }, { updatedAt: "desc" }],
+        });
+    } else {
+        streams = await db.stream.findMany({
+            select: {
+                id: true,
+                user: true,
+                thumbnailUrl: true,
+                name: true,
+                isLive: true,
+            },
+            orderBy: [{ isLive: "desc" }, { updatedAt: "desc" }],
+        });
+    }
+
+    return streams;
+}
